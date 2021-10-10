79279296ec2f95e5c6051a42151a73dc6b3a9ee5
briiiqtt <briiiqtt@gmail.com>
2021-10-11 02:09:37 +0900

    Revert "commit"
    This reverts commit 460b82d1c02cefa6b23a9a0adeb8e2d9ab1ac3fe.

---
+++ Changho_AJAX/AJAX_WebServer/src/co/yedam/common/Employee.java
@@ -0,0 +1,40 @@
+package co.yedam.common;
+
+public class Employee {
+	private int employeeId;
+	private String lastName;
+	private String Email;
+	private String jobId;
+	private String hireDate;
+	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getJobId() {
+		return jobId;
+	}
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+	public String getHireDate() {
+		return hireDate;
+	}
+	public void setHireDate(String hireDate) {
+		this.hireDate = hireDate;
+	}
+}
---
+++ Changho_AJAX/AJAX_WebServer/src/co/yedam/common/TempMain.java
@@ -0,0 +1,10 @@
+package co.yedam.common;
+
+public class TempMain {
+
+	public static void main(String[] args) {
+		EmpDAO dao = new EmpDAO();
+		dao.getEmpList();
+	}
+
+}
