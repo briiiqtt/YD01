be40cd16061c08f127765c07ff0293c90a756704
briiiqtt <briiiqtt@gmail.com>
2021-10-13 01:52:30 +0900

    2021.10/12

---
+++ commentProject2222/src/co/yedam/chart/EmpDAO.java
@@ -0,0 +1,38 @@
+package co.yedam.chart;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import co.yedam.common.DAO;
+
+public class EmpDAO extends DAO {
+   public Map<String, Integer> getDeptByEmp() {
+      
+      connect();
+      Map<String, Integer> map = new HashMap<String, Integer>();
+      String sql = "select d.department_name, count(1)\r\n"
+               + "from employees e\r\n"
+               + "join departments d\r\n"
+               + "on e.department_id = d.department_id\r\n"
+               + "group by d.department_name";
+      
+      //데이터 있거나 , 에러 == > false , 없는게 확실 =>true;
+      try {
+         psmt=conn.prepareStatement(sql);
+         rs=psmt.executeQuery();
+         while(rs.next()){
+            map.put(rs.getString(1), rs.getInt(2));
+         }
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }finally {
+         disconnect();
+      }
+      return map;
+      
+   }
+   
+}
