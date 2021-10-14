875ec6809e1b3582f9645152800d8327c2f61290
briiiqtt <briiiqtt@gmail.com>
2021-10-15 01:21:37 +0900

    lesson

---
+++ commentProject2222/src/brq/newlec/filter/ServletFilter.java
@@ -0,0 +1,37 @@
+package brq.newlec.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+@WebFilter("/*")
+public class ServletFilter implements Filter{
+
+	@Override
+	public void destroy() {
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		System.out.println("Requested");
+		chain.doFilter(request, response);
+		System.out.println("Responded");
+		
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		
+	}
+
+}
